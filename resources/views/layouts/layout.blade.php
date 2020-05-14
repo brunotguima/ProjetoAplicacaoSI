295c73d7567247416b3a4f4c8a3ad83f8166ee3e
Bruno Guimarães <brunotguima@alunos.fho.edu.br>
2020-05-14 18:00:32 -0300

    Editing Veiculo + alter layout

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,12 +5,68 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Sistema de Controle de Frota</title>
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+  <link href="{{ asset('css/semantic.css') }}" rel="stylesheet" type="text/css" />
 </head>
 <body>
-  <div class="container">
-    @yield('content')
+  <header class="ui header">
+<div class="ui container">
+  <!-- Menu superior -->
+  @if(Auth::check())
+<div class="ui pointing menu">
+  <a href="/" class="active item" id="inicio">
+    Inicio
+  </a>
+  <a href="/veiculos" class="item" id="veiculos">
+    Veiculos
+  </a>
+  <a href="/movimentacao" class="item" id="movimentacoes">
+    Movimentações
+  </a>
+  <div class="right menu">
+    <button class="ui secondary button">
+    <a href="">{{Auth::user()->name}}</a>
+    </button>
+<form action="/logout" method="post">
+  <input type="submit" class="ui secondary basic button" value="Logout">
+  {{ csrf_field() }}
+</form>
   </div>
-  <script src="{{ asset('js/app.js') }}" type="text/js"></script>
+</div>
+@endif
+</div>
+</header>
+  <!--Fim menu superior -->
+  <main>
+  <div class="ui container">
+    @if(Auth::Check())
+    @yield('content')   
+   @else
+       <div class="ui container">
+        <div class="ui vertically grid">
+          <div class="row"> </div>
+           <div class="row">
+            <img class="ui small rounded centered image" src="/Images/logo.png" sizes="150px">
+           </div>
+           <div class="two column row">
+            <button class="fluid ui massive primary basic button"> <a href="{{route('login')}}">Faça o Login</a></button>
+            <button class="fluid ui massive positive basic button"><a href="{{route('register')}}">Registre-se</a></button>
+          </div>
+          <div class="four column row"></div>
+         </div>
+       </div>
+   @endif
+  </div>
+  </main>
+  <footer>
+
+  </footer>
+  <script src="{{ asset('js/semantic.js') }}" type="text/js"></script>
+  <script>
+    var url_atual = window.location.href;
+    if(url_atual == 'localhost:8000/veiculos'){
+      $('#inicio').removeClass('active');
+      $('#veiculos').addClass('active');
+    }
+  </script>
 </body>
 </html>
--- resources/views/veiculos/index.blade.php
+++ resources/views/veiculos/index.blade.php
@@ -2,4 +2,45 @@
 
 @section('content')
 
-{{dd($veiculosCadastrados)}}
+<table class="ui selectable table">
+    <thead>
+      <tr>
+        <th>QRCode</th>
+        <th>Tipo</th>
+        <th>Marca</th>
+        <th>Modelo</th>
+        <th>Ano</th>
+        <th>Cor</th>
+        <th>Placa</th>
+        <th>Tanque</th>
+        <th>Renavam</th>
+        <th>KM Cadastro</th>
+        <th>KM Atual</th>
+        <th class="right aligned"></th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($veiculosCadastrados as $veiculo)
+        <tr>
+        <td><a href=""><i>a</i></a></td>
+            <td>{{$veiculo->tipo}}</td>
+            <td>{{$veiculo->marca}}</td>
+            <td>{{$veiculo->modelo}}</td>
+            <td>{{$veiculo->ano}}</td>
+            <td>{{$veiculo->cor}}</td>
+            <td>{{$veiculo->placa}}</td>
+            <td>{{$veiculo->tanque}}</td>
+            <td>{{$veiculo->renavam}}</td>
+            <td>{{$veiculo->kmcadastro}}</td>
+            <td>{{$veiculo->kmatual}}</td>
+            <td class="right aligned">
+                <button class="orange basic button ui"> <a href="{{route('veiculos.show',$veiculo->id)}}">Ver</a></button>
+                <button class="green basic button ui"> <a href="{{route('veiculos.edit',$veiculo->id)}}">Editar</a></button>
+                <button class="red basic button ui"> <a href="{{route('veiculos.destroy',$veiculo->id)}}">Remover</a></button>
+
+            </td>
+          </tr>
+        @endforeach
+    </tbody>
+  </table>
+
