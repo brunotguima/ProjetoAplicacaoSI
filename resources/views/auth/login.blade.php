d6b09715b139d5f97a45383d35875889b7833157
Matheus Oliveira <m.loliveira@alunos.fho.edu.br>
2020-05-25 12:04:16 -0300

    locale pt_BR

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,54 +1,55 @@
 @extends('layouts.layout')
 
 @section('content')
-<!DOCTYPE html>
-<html>
+<div class="ui middle aligned center aligned grid">
+    <div class="column" style="max-width: 450px; margin-top: 20px">
+        <h2 class="ui teal center aligned header">
+            Login
+        </h2>
+        <form class="ui large form" method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="ui stacked segment">
+                <div class="field">
+                    <div class="ui left icon input">
+                        <i class="user icon"></i>
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                            name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-<head>
-    <div class="ui middle aligned center aligned grid">
-        <div class="column" style="max-width: 450px; margin-top: 20px">
-            <h2 class="ui teal center aligned header">
-                Login
-            </h2>
-            <form class="ui large form" method="POST" action="{{ route('login') }}">
-                @csrf
-                <div class="ui stacked segment">
-                    <div class="field">
-                        <div class="ui left icon input">
-                            <i class="user icon"></i>
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                            @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
                     </div>
-                    <div class="field">
-                        <div class="ui left icon input">
-                            <i class="lock icon"></i>
-                            <input id="password" type="password"
-                                class="form-control @error('password') is-invalid @enderror" name="password" required
-                                autocomplete="current-password">
-
-                            @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                    </div>
-                    <div class="ui fluid large teal submit button">Login</div>
                 </div>
-                <div class="ui error message"></div>
-            </form>
-        </div>
-    </div>
+                <div class="field">
+                    <div class="ui left icon input">
+                        <i class="lock icon"></i>
+                        <input id="password" type="password"
+                            class="form-control @error('password') is-invalid @enderror" name="password" required
+                            autocomplete="current-password">
 
-    <script>
-        $(document).ready(function () {
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="ui fluid large teal submit button">Login</div>
+            </div>
+            @if (Route::has('password.request'))
+            <a href="{{ route('password.request') }}">
+                {{ __('Esqueceu a senha?') }}
+            </a>
+            @endif
+            <div class="ui error message"></div>
+        </form>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function () {
       $(".ui.form").form({
         fields: {
           email: {
@@ -78,8 +79,8 @@
         }
       });
     });
-    </script>
-    </body>
+</script>
+</body>
 
 </html>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,94 @@
-@extends('layouts.app')
+@extends('layouts.layout')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<div class="ui middle aligned center aligned grid">
+    <div class="column" style="max-width: 450px; margin-top: 20px">
+        <h2 class="ui teal center aligned header">
+            Recuperar Senha
+        </h2>
+        <form class="ui large form" method="POST" action="{{ route('password.update') }}">
+            @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+            <input type="hidden" name="token" value="{{ $token }}">
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+            <div class="ui stacked segment">
+                <div class="field">
+                    <div class="ui left icon input">
+                        <i class="user icon"></i>
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                            name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
                 </div>
+                <div class="field">
+                    <div class="ui left icon input">
+                        <i class="lock icon"></i>
+                        <input id="password" type="password"
+                            class="form-control @error('password') is-invalid @enderror" name="password" required
+                            autocomplete="current-password">
+
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="ui left icon input">
+                        <i class="lock icon"></i>
+                        <input id="password" type="password" name="password_confirmation" required
+                            autocomplete="current-password">
+
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+                <input type="submit" class="ui fluid large teal submit button" value="Login">
             </div>
-        </div>
+            <div class="ui error message"></div>
+        </form>
     </div>
 </div>
+
+<script>
+    $(document).ready(function () {
+      $(".ui.form").form({
+        fields: {
+          email: {
+            identifier: "email",
+            rules: [{
+                type: "empty",
+                prompt: "Digite seu e-mail"
+              },
+              {
+                type: "email",
+                prompt: "Por favor, digite um e-mail válido"
+              }
+            ]
+          },
+          password: {
+            identifier: "password",
+            rules: [{
+                type: "empty",
+                prompt: "Por favor, digite sua senha"
+              },
+              {
+                type: "length[6]",
+                prompt: "Sua senha deve ter no mínimo, 6 caracteres"
+              }
+            ]
+          }
+        }
+      });
+    });
+</script>
 @endsection
